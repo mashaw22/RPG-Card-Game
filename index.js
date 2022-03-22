490675c9615b47b3fd75211a67dcf715f96f8c69
Samantha Neely <samneely.dev@gmail.com>
2022-03-21 21:23:50 -0500

    Updated Javascript gameplay

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 import characterData from "/data.js"
 import Character from "/Character.js"
 
+let monstersArray = ["orc", "demon", "goblin"]
+let isWaiting = false
+
+function getNewMonster() {
+    const nextMonsterData = characterData[monstersArray.shift()]
+    return nextMonsterData ? new Character(nextMonsterData) : {}
+}
+
 function attack() {
-    wizard.getDiceHtml()
-    orc.getDiceHtml()
-    render()
+    if(!isWaiting){
+        wizard.setDiceHtml()
+        monster.setDiceHtml()
+        wizard.takeDamage(monster.currentDiceScore)
+        monster.takeDamage(wizard.currentDiceScore)
+        render()
+        if (wizard.dead) {
+            endGame()
+        } 
+        else if (monster.dead) {
+            if(monstersArray.length > 0) {
+                isWaiting = true
+                setTimeout(()=> {
+                    monster = getNewMonster()
+                    render()
+                    isWaiting = false
+                }, 1000)
+            } 
+            else {
+                endGame()
+            }
+        }
+    }
+}
+
+function endGame() {
+    isWaiting = true
+    const endMessage = wizard.health === 0 && monster.health === 0 ?
+    "No victors - all creatures are dead." : 
+    wizard.health > 0 ? "The Wizard Wins" : 
+        "The monsters are victorious"
+
+    const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
+    setTimeout(()=> {
+        document.body.innerHTML = `
+            <div class="end-game">
+                <h2>Game Over</h2>
+                <h3>${endMessage}</h3>
+                <p class="end-emoji">${endEmoji}</p>
+            </div>
+            `
+    },1500)
 }
 
 function render() {
     document.getElementById("hero").innerHTML = wizard.getCharacterHtml()
-    document.getElementById("monster").innerHTML = orc.getCharacterHtml()
+    document.getElementById("monster").innerHTML = monster.getCharacterHtml()
 }
 document.getElementById("attack-button").addEventListener("click", attack)
 const wizard = new Character(characterData.hero)
-const orc = new Character(characterData.monster)
+let monster = getNewMonster()
 render()
